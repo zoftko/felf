eb33c8e7fb72d8159f32e9aca9812e85ae912b17
Antonio <antonioglez-23@hotmail.com>
2023-10-13 15:39:46 -0600

    build: initial commit
    A skeleton as obtained from Spring Boot's initializer service has been
    added. This is the minimal code to start an application and run a CI
    pipeline on it.

---
+++ src/main/java/com/zoftko/felf/FelfApplication.java
@@ -0,0 +1,12 @@
+package com.zoftko.felf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FelfApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zoftko/felf/FelfApplicationTests.java
@@ -0,0 +1,11 @@
+package com.zoftko.felf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FelfApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}

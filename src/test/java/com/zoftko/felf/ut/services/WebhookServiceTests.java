cbfb95d6046b9009a2192f33db38ec6927035dcc
CrazyBolillo <antonio@zoftko.com>
2023-10-30 21:35:53 -0600

    feat: add daisy.ui, frontend revamp
    * daisy.ui has been added to speed up development and improve the looks
      of the website with its styling.
    * components have been split into fragments code reuse
    
    The frontend also has some style and design improvements as well, for
    example:
    
    * user's avatar shown on navbar
    * dropdown on avatar click, enable logout
    * added logo and favicon
    * dashboard shows dropdown with all app installations available to the
      user

--- src/test/java/com/zoftko/felf/ut/services/WebhookServiceTests.java
+++ src/test/java/com/zoftko/felf/ut/services/WebhookServiceTests.java
@@ -1,6 +1,6 @@
 package com.zoftko.felf.ut.services;
 
-import static com.zoftko.felf.services.WebhookService.ACTION_DELETED;
+import static com.zoftko.felf.services.WebhookService.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -12,11 +12,15 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 
 @DataJpaTest
+@ExtendWith(OutputCaptureExtension.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class WebhookServiceTests {
 
@@ -94,5 +98,32 @@ class WebhookServiceTests {
             )
         );
         assertThat(installationRepository.findById(installationId)).isNotPresent();
+    }
+
+    @Test
+    void processBadPayload(CapturedOutput output) throws JSONException, JsonProcessingException {
+        int installationId = 12345;
+        webhookService.processEvent(
+            EVENT_INSTALLATION,
+            objectMapper.readTree(
+                new JSONObject()
+                    .put("action", ACTION_CREATED)
+                    .put("installation", new JSONObject().put("what", "boom").put("id", installationId))
+                    .toString()
+            )
+        );
+
+        assertThat(output.getOut()).contains("invalid payload for installation action");
+        assertThat(installationRepository.findById(installationId)).isNotPresent();
+    }
+
+    @Test
+    void processBadAction(CapturedOutput output) throws JSONException, JsonProcessingException {
+        webhookService.processEvent(
+            EVENT_INSTALLATION,
+            objectMapper.readTree(new JSONObject().put("action", "bazinga").toString())
+        );
+
+        assertThat(output.getOut()).contains("unknown installation action");
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   content: ["./src/main/resources/templates/**/*.html"],
   theme: {
@@ -9,6 +7,34 @@ module.exports = {
   corePlugins: {
     preflight: true,
   },
-  plugins: [],
+  plugins: [
+      require("@tailwindcss/typography"),
+      require("daisyui")
+  ],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+          ".bg-surface-100": {
+            "background-color": "hsl(0 0% 98%)"
+          },
+          ".border-surface-100": {
+            "border-color": "hsl(0 0% 90%)"
+          },
+        }
+      },
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          ".bg-surface-100": {
+            "background-color": "hsl(212 18% 16%)"
+          },
+          ".border-surface-100": {
+            "border-color": "hsl(212 18% 18%)"
+          },
+        }
+      },
+    ]
+  }
 }
-

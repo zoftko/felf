0be70bbf8058624602c18d3fd6e7f96ba1f55e4a
CrazyBolillo <antonio@zoftko.com>
2023-11-03 11:25:14 -0600

    feat: add surface color
    Surface color has been added to highlight elements which go above the bg
    color.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,4 +11,30 @@ module.exports = {
       require("@tailwindcss/typography"),
       require("daisyui")
   ],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+          ".bg-surface-100": {
+            "background-color": "hsl(0 0% 98%)"
+          },
+          ".border-surface-100": {
+            "border-color": "hsl(0 0% 90%)"
+          },
+        }
+      },
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          ".bg-surface-100": {
+            "background-color": "hsl(212 18% 16%)"
+          },
+          ".border-surface-100": {
+            "border-color": "hsl(212 18% 18%)"
+          },
+        }
+      },
+    ]
+  }
 }

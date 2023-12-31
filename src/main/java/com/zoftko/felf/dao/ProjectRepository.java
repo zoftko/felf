c7162a6d94b72b93cbbddf660eac983f7a5278ff
CrazyBolillo <antonio@zoftko.com>
2023-12-31 14:03:32 -0600

    feat: support repository update webhook
    Repository webhook events are now handled in order to stay up to date on
    the visibility and default branches of repos.
    
    Related to #8.

--- src/main/java/com/zoftko/felf/dao/ProjectRepository.java
+++ src/main/java/com/zoftko/felf/dao/ProjectRepository.java
@@ -3,8 +3,19 @@ package com.zoftko.felf.dao;
 import com.zoftko.felf.entities.Project;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
     Optional<Project> findByFullName(String fullName);
     Optional<Project> findByToken(String token);
+
+    @Modifying
+    @Query("UPDATE Project p SET p.defaultBranch = :branch WHERE p.fullName = :name")
+    void updateDefaultBranchByFullName(@Param("name") String fullName, @Param("branch") String branch);
+
+    @Modifying
+    @Query("UPDATE Project p SET p.isPrivate = :private WHERE p.fullName = :name")
+    void updateIsPrivateByFullName(@Param("name") String fullName, @Param("private") boolean isPrivate);
 }

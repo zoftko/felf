cfe063701d0b2f947f8b394b2be583d09c744c72
CrazyBolillo <antonio@zoftko.com>
2024-01-02 11:54:32 -0600

    feat: public project pages
    Pages are now public and can be seen by visitors that are not logged in.
    Login and logout handlers were updated in order to take users back to
    the original page they were located before logging in/out.
    
    Closes #9.

--- src/main/java/com/zoftko/felf/controllers/StaticController.java
+++ src/main/java/com/zoftko/felf/controllers/StaticController.java
@@ -1,5 +1,7 @@
 package com.zoftko.felf.controllers;
 
+import com.zoftko.felf.config.SecurityConfiguration;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +13,18 @@ public class StaticController {
     public static final String LOGIN_MAPPING = "/login";
 
     @GetMapping(LOGIN_MAPPING)
-    public String login(@RequestParam(name = "signup", required = false) String signUp, Model model) {
+    public String login(
+        HttpServletRequest request,
+        @RequestParam(name = "signup", required = false) String signUp,
+        Model model
+    ) {
         String message;
         if (signUp != null) {
             message = "No signup required, simply log in with your GitHub account to get started";
         } else {
+            request
+                .getSession()
+                .setAttribute(SecurityConfiguration.SESSION_REDIRECT_ATTR, request.getHeader("Referer"));
             message = "Welcome back, please log in";
         }
         model.addAttribute("welcomeMessage", message);
